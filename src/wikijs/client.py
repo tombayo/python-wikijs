7f8b5677d498f45e8ebf572a6fb750394681fd22
Tom-Andre Munkhaug <tom.a.munkhaug@nte.no>
2024-09-18 18:02:47 +0200

    Added the ability to modify the automatic schema-fetching when creating the api-client.

--- src/wikijs/client.py
+++ src/wikijs/client.py
@@ -22,15 +22,16 @@ log = logging.getLogger(__name__)
 
 
 class WikiJs(AssetMixin, PageMixin, SystemMixin, UserMixin):
-    def __init__(self, endpoint, api_key) -> None:
+    def __init__(self, endpoint, api_key, schema_fetch=True) -> None:
         self.endpoint = endpoint
         self.api_key = api_key
+        self.schema_fetch = schema_fetch
 
     @cached_property
     def client(self) -> Client:
         headers = {'Authorization': f'Bearer {self.api_key}'}
         transport = AIOHTTPTransport(url=self.endpoint, headers=headers)
-        return Client(transport=transport, fetch_schema_from_transport=True)
+        return Client(transport=transport, fetch_schema_from_transport=self.schema_fetch)
 
     def execute(self, query: str, params: 'Optional[Dict[str, Any]]' = None) -> Any:
         log.debug('Query: %s / Params: %s', query, params)
